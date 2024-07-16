73241cf4092a98fad9564ef8f89577b2d36ad609
kkyuheak <gyuheak@gmail.com>
2024-07-16 15:46:26 +0900

    motion values

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,55 @@
 import styled from "styled-components";
-import { Variants, motion } from "framer-motion";
+import {
+  Variants,
+  motion,
+  useMotionValue,
+  useScroll,
+  useTransform,
+} from "framer-motion";
+import { useEffect, useRef } from "react";
 
 const Wrapper = styled.div`
-  height: 100vh;
+  height: 500vh;
   width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
+const BigBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`;
+
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: rgba(255, 255, 255, 1);
   border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
   display: grid;
   grid-template-columns: repeat(2, 1fr);
 `;
 
-const Circle = styled(motion.div)`
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  background-color: #fff;
-  height: 70px;
-  width: 70px;
-  place-self: center;
-  border-radius: 50%;
-`;
-
 const boxVars: Variants = {
-  start: { scale: 0.5, opacity: 0 },
-  end: {
-    scale: 1,
-    opacity: 1,
-    transition: {
-      type: "spring",
-      duration: 0.5,
-      bounce: 0.5,
-      // delayChildren: 0.5,
-      staggerChildren: 0.2,
-    },
-  },
+  hover: { scale: 1.5, rotateZ: 90 },
+  tap: { borderRadius: "50%", scale: 1 },
 };
 
-const circleVars = {
-  start: { opacity: 0, y: 10 },
-  end: {
-    opacity: 1,
-    y: 0,
-  },
-};
 const App = () => {
+  const x = useMotionValue(0);
+  const { scrollYProgress } = useScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
+
   return (
     <Wrapper>
-      <Box variants={boxVars} initial="start" animate="end">
-        <Circle variants={circleVars} />
-        <Circle variants={circleVars} />
-        <Circle variants={circleVars} />
-        <Circle variants={circleVars} />
-      </Box>
+      <Box drag="x" dragSnapToOrigin style={{ x, scale }}></Box>
     </Wrapper>
   );
 };
