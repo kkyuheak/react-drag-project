421e66721a81f3c002ba95b7a6efb079b1ed276f
kkyuheak <gyuheak@gmail.com>
2024-07-17 21:51:22 +0900

    click overlay

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,64 @@
 import styled from "styled-components";
-import { AnimatePresence, Variants, motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
   width: 100vw;
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
 `;
 
 const Box = styled(motion.div)`
-  width: 400px;
-  height: 200px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
-  position: absolute;
-  top: 100px;
+  height: 200px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 50vw;
+  gap: 10px;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
+`;
+
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.7);
+  position: absolute;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
-const boxVariants: Variants = {
-  initial: (back: boolean) => ({
-    opacity: 0,
-    scale: 0,
-    x: back ? -500 : 500,
-  }),
-  visible: {
-    opacity: 1,
-    scale: 1,
-    x: 0,
-    transition: { duration: 1 },
-  },
-  leaving: (back: boolean) => ({
-    opacity: 0,
-    scale: 0,
-    x: back ? 500 : -500,
-    transition: { duration: 1 },
-  }),
-};
-
 const App = () => {
-  const [visible, setVisible] = useState(1);
-  const [back, setBack] = useState(false);
-  const handleNextClick = () => {
-    setBack(false);
-    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
-  };
-  const handlePrevClick = () => {
-    setBack(true);
-    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
-  };
+  const [id, setId] = useState<null | string>(null);
+
   return (
     <Wrapper>
-      <AnimatePresence custom={back}>
-        <button onClick={handlePrevClick}>{"<"}</button>
-
-        <Box
-          custom={back}
-          variants={boxVariants}
-          initial="initial"
-          animate="visible"
-          exit="leaving"
-          key={visible}
-        >
-          {visible}
-        </Box>
-
-        <button onClick={handleNextClick}>{">"}</button>
+      <Grid>
+        {["1", "2", "3", "4"].map((n) => (
+          <Box onClick={() => setId(n)} key={n} layoutId={n} />
+        ))}
+      </Grid>
+      <AnimatePresence>
+        {id ? (
+          <Overlay
+            onClick={() => setId(null)}
+            initial={{ backgroundColor: "rgba(0,0,0,0)" }}
+            animate={{ backgroundColor: "rgba(0,0,0,0.5)" }}
+            exit={{ backgroundColor: "rgba(0,0,0,0)" }}
+          >
+            <Box layoutId={id} style={{ width: 500, height: 300 }} />
+          </Overlay>
+        ) : null}
       </AnimatePresence>
     </Wrapper>
   );
